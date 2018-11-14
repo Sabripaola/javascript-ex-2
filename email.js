3de94b6197c645c6719b4bc70cb7a11095438ffd
Sabrina Paola <sabripaola1808@gmail.com>
2018-11-14 18:36:36 +0100

    email

---
+++ email.js
@@ -0,0 +1,21 @@
+var email= prompt("inserire email");
+var us_email = ["uno@gmail.com","due@gmail.com","tre@gmail.com"];
+
+
+for (var i = 0; i < us_email.length; i++) {
+  var utente_email = us_email[i];
+
+  if (email==utente_email) {
+    var lista_email=0;
+  }
+
+  var lista_email;
+
+  if (lista_email===0) {
+    alert("accesso effettuato");
+  }
+
+  else{
+    alert ("accesso errato");
+  }
+}
